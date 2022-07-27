aab9807f99e8b71d55c34e2ab345eed92714fffd
Gavin Carew <gjcarew@gmail.com>
2022-07-27 08:06:49 -0700

    Finish museum tests for iteration 2

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -20,6 +20,21 @@ describe Museum do
     expect(@dmns.exhibits).to eq([])
   end
 
-  it 
+  it 'can add exhibits' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end
+
+  it 'recommends exhibits that match patron interests' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
+  end
 
 end
