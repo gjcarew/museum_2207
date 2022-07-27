1f408e5d24e808e93e9725c346bb1d76365d18f2
Gavin Carew <gjcarew@gmail.com>
2022-07-27 08:00:03 -0700

    Set up tests for iteration 2

---
+++ spec/museum_spec.rb
@@ -0,0 +1,25 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+describe Museum do
+  before :each do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+  end
+
+  it 'has attributes' do
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
+
+  it 
+
+end
