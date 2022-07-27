d0a3affa161c5b7906ba1dc561c1ca7b449ec22c
Gavin Carew <gjcarew@gmail.com>
2022-07-27 07:51:07 -0700

    Add tests for patron class

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,2 +1,8 @@
 class Exhibit
+  attr_reader :name, :cost
+
+  def initialize(init_hash)
+    @name = init_hash[:name]
+    @cost = init_hash[:cost]
+  end
 end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,25 @@
+require './lib/patron'
+
+RSpec.describe Patron do
+  before :each do
+    @patron_1 = Patron.new("Bob", 20)
+
+  it 'exists' do
+    expect(@patron_1).to be_a Patron
+  end
+
+  it 'has attributes' do
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
+    expect(@patron_1.interests).to be_a Hash
+    expect(@patron_1.interests).to be_empty
+  end
+
+  it 'can add interests' do
+    expect(@patron_1.interests).to be_empty
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+end
